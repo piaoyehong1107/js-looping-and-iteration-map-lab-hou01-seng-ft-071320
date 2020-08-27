ec6a20f5ead276d521717811a4d6857f5b47db0c
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-27 21:01:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+  const newdriver=[]
+  drivers.map (function(driver){
+     newdriver.push(driver.toLowerCase())
+  })
+  return newdriver
+}
+
+function nameToAttributes(drivers){
+  const newdriver=[]
+  drivers.map (function(driver){
+    let name=driver.split(" ")
+    newdriver.push({firstName:name[0],lastName:name[1]})
+  })
+  return newdriver
+}
+
+function attributesToPhrase(drivers){
+  const newdriver=[]
+  drivers.map (function(driver){
+   newdriver.push(`${driver.name} is from ${driver.hometown}`)
+  })
+  return newdriver
+}
