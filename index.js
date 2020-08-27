a3c4b8e52cd8136d265bb89ce87f03745f1e41b5
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-27 20:58:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function nameToAttributes(drivers){
   })
   return newdriver
 }
+
+function nameToAttributes(drivers){
+  const newdriver=[]
+  drivers.map (function(driver){
+    let name=driver.split(" ")
+    newdriver.push({firstName:name[0],lastName:name[1]})
+  })
+  return newdriver
+}
